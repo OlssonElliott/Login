b1566d0289eed932e7976a8bb8bc6afc03f1ed58
Elliott <sendtoelle@hotmail.com>
2025-01-06 17:50:46 +0100

    added models, connected to sql database

--- login/src/main/java/com/login/login/models/Product.java
+++ login/src/main/java/com/login/login/models/Product.java
@@ -1,5 +1,65 @@
 package com.login.login.models;
 
-public class Product {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String pictureLink;
+    private String description;
+    private String price;
+
+    public Product(String name, String pictureLink, String description, String price) {
+        this.name = name;
+        this.pictureLink = pictureLink;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureLink() {
+        return pictureLink;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPictureLink(String pictureLink) {
+        this.pictureLink = pictureLink;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
