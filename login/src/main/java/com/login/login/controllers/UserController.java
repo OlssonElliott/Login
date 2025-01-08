c4707b2c15930d14c250bd2b105bc467a83c6a60
Elliott <sendtoelle@hotmail.com>
2025-01-08 10:59:10 +0100

    added buttons to homepage

--- login/src/main/java/com/login/login/controllers/UserController.java
+++ login/src/main/java/com/login/login/controllers/UserController.java
@@ -6,7 +6,6 @@ import com.login.login.models.User;
 import com.login.login.repositories.LoginRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
